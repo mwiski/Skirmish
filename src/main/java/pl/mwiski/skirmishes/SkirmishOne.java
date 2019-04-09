d2c31958b9af85460029cc0398aa4ae136506fe4
mwiski <49036916+mwiski@users.noreply.github.com>
2019-04-09 16:09:00 +0200

    skimrish: added new Integers (#1)
    * skimrish: added new Integers
    
    * skirmish1: changed method removeDuplicates and added class SkirmishApplication

--- src/main/java/pl/mwiski/skirmishes/SkirmishOne.java
+++ src/main/java/pl/mwiski/skirmishes/SkirmishOne.java
@@ -4,25 +4,16 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-public class SkirmishOne {
+class SkirmishApplication {
 
-    private static List<Integer> removeDuplicates(List<Integer> ints) {
-
-        Set<Integer> set = new HashSet<>();
-        set.addAll(ints);
-        ints.clear();
-        ints.addAll(set);
-        return ints;
-    }
-
-    private static List<Integer> init() {
+    public List<Integer> init() {
         List<Integer> ints = new ArrayList<>();
 
         ints.add(1);
         ints.add(0);
         ints.add(2);
         ints.add(3);
-        ints.add(4);
+        ints.add(5);
         ints.add(0);
         ints.add(1);
         ints.add(3);
@@ -33,14 +24,27 @@ public class SkirmishOne {
         ints.add(0);
         ints.add(2);
         ints.add(5);
+        ints.add(6);
+        ints.add(4);
+        ints.add(6);
         ints.add(5);
+
+        return ints;
+    }
+}
+
+public class SkirmishOne {
+
+    private static List<Integer> removeDuplicates(List<Integer> ints) {
+        ints = new ArrayList<>(new LinkedHashSet<>(ints));
 
         return ints;
     }
 
     public static void main (String[]args){
 
-        System.out.println(removeDuplicates(init()));
-    }
+    SkirmishApplication application = new SkirmishApplication();
 
+    System.out.println(removeDuplicates(application.init()));
+    }
 }
